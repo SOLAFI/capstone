6db1e5a2f4d2f34c62a3dda262f1656ee3fa8a4d
Xue Zhao <402949621@qq.com>
2021-06-27 10:33:39 +0800

    Revert "Revert "New Custom Widget - FloatingBackButton""
    This reverts commit baf644de0ff60668643c24423159020f762d94cd.

---
+++ lib/widgets/buttons.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class FloatingBackButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      onPressed: ()=> Navigator.of(context).pop(),
+      child: Icon(Icons.arrow_back),
+    );
+  }
+}
